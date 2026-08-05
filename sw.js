f6f23142a5b71b665200b5a63d41597af591b164
Stoa76 <buzzkjs@gmail.com>
2026-08-05 14:05:36 +0900

    Update cache version to 'bed-finder-v1.0.1'

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bed-finder-v1';
+const CACHE='bed-finder-v1.0.1';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(
